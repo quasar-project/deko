1ad48dda039f8ef646754179c1e7b39632db2be9
whs31 <75864292+whs31@users.noreply.github.com>
2024-01-04 00:23:55 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 import os
+from sys import platform
 
 if __name__ == "__main__":
     os.system("cargo build --release")
-    os.system("cd target")
-    os.mkdir("dll")
-    
+    os.chdir("target")
+    os.system("mkdir dll")
+    if platform == "linux":
+        os.system("cp ./release/libdeko.so ./dll")
+    if platform == "win32":
+        os.system("copy .\\release\\deko.dll .\\dll\\deko.dll")
