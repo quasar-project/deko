04cac300c325fce5fd9b2783552e0c412c05e93f
whs31 <75864292+whs31@users.noreply.github.com>
2024-01-03 21:51:17 +0300

    Examples and test binary

--- src/bin/deko_cli/main.rs
+++ src/bin/deko_cli/main.rs
@@ -5,30 +5,23 @@ fn main()
   println!("➡️ starting cli...");
   deko::init_logger("trace")
     .expect("failed to initialize logger");
-  let path = env::current_dir()
-    .unwrap()
-    .join("m1-13-12-2022_20-53-26.jpg")
-    .into_os_string()
-    .into_string()
-    .unwrap();
-  let path1 = env::current_dir()
-    .unwrap()
-    .join("m1-13-12-2022_20-51-15.jpg")
-    .into_os_string()
-    .into_string()
-    .unwrap();
-  let path2 = env::current_dir()
-    .unwrap()
-    .join("m3-13-12-2022_21-26-14.jpg")
-    .into_os_string()
-    .into_string()
-    .unwrap();
+  // scan current dir for jpeg files
+  let mut jpegs: Vec<String> = Vec::new();
+  for entry in std::fs::read_dir(env::current_dir().unwrap()).unwrap() {
+    let path = entry.unwrap().path();
+    if path.is_file() {
+      let ext = path.extension();
+      if ext.is_some() && ext.unwrap() == "jpg" {
+        jpegs.push(path.into_os_string().into_string().unwrap());
+      }
+    }
+  }
   let mut decoder = deko::decoder::jpeg_decoder::JPEG_DECODER
     .lock()
     .unwrap();
   decoder.set_config(deko::Config::default());
-  decoder.decode_file(&path).unwrap();
-  decoder.decode_file(&path1).unwrap();
-  decoder.decode_file(&path2).unwrap();
+  for jpeg in jpegs {
+    decoder.decode_file(&jpeg).unwrap();
+  }
   println!("☑️ cli finished running!");
 }
