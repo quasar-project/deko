a65cf9b357a1e507f0045b0abe15cdfd64f26ddf
whs31 <75864292+whs31@users.noreply.github.com>
2024-01-04 00:38:09 +0300

    simple cmake

--- build.py
+++ build.py
@@ -11,3 +11,17 @@ if __name__ == "__main__":
         os.system("cp ./release/libdeko.so ./dll")
     if platform == "win32":
         os.system("copy .\\release\\deko.dll .\\dll\\deko.dll")
+    os.chdir("../")
+    os.chdir("bindings/c++")
+    os.system("mkdir plugins")
+    os.chdir("plugins")
+    if platform == "linux":
+        os.system("mkdir Linux")
+        os.chdir("Linux")
+        os.system("mkdir x64")
+        os.system("cp ../../../../target/dll/libdeko.so ./x64/libdeko.so")
+    if platform == "win32":
+        os.system("mkdir Windows")
+        os.chdir("Windows")
+        os.system("mkdir x64")
+        os.system("copy ..\\..\\..\\..\\target\\dll\\deko.dll .\\x64\\deko.dll")
